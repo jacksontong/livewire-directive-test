a75d425d7f00c2b181f2bdc26f9f61d15f4e06a9
Jackson Tong <tongtoan2704@gmail.com>
2023-04-18 08:54:16 +0700

    example livewire directive is not working

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex flex-col space-y-6 p-3">
+                <h3>Test Livewire custom directive</h3>
+
+                <div x-data="test" x-text="text"></div>
+
+                <div wire:sortable>
+                    Sortable livewire directive
+                </div>
             </div>
         </div>
+        <script>
+            document.addEventListener('alpine:init', () => {
+                Alpine.data('test', () => ({
+                    text: 'From Alpine component'
+                }));
+            });
+            document.addEventListener('alpine:init', () => {
+                Livewire.directive('sortable', (el) => {
+                    console.log('sortable', el); // no output on console
+                });
+            });
+        </script>
     </div>
 </x-app-layout>
